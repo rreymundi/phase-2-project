ab1aa8664c402951ea609608e508e3137811ce8b
Raúl Reymundi <raul@kickstarter.com>
2022-08-26 15:36:38 -0400

    set up my routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import NavBar from "./components/NavBar";
 import Home from "./components/Home";
+import NewRecipe from "./components/NewRecipe";
+import SavedRecipes from "./components/SavedRecipes";
 
 const App = () => {
   return (
-    <div>
-      <Home />
-    </div>
+    <Router>
+      <NavBar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/recipes/saved' element={<SavedRecipes />} />
+        <Route path='/recipes/new' element={<NewRecipe />} />
+      </Routes>
+    </Router>
   );
 }
 
